89189c6cb26fdfb5f945e279e9ef164ad50881d4
hibikihsh <hydrogen1121@gmail.com>
2025-09-17 18:51:12 +0900

    feat(HandlingUserInput): add user input handling component with message manipulation

---
+++ src/components/HandlingUserInput.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+const message = ref("Reverse Message");
+
+const reverseMessage = () => {
+  message.value = message.value.split("").reverse().join("");
+};
+
+const appendExclamation = () => {
+  message.value = message.value + "!";
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="reverseMessage">Reverse Message</button>
+  <button @click="appendExclamation">Append '!'</button>
+</template>
